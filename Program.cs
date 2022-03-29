e35d21feeb03d9b1b40859f3ba2b5046e18a3501
Baesa <quebedesgamer@gmail.com>
2022-03-29 14:28:47 -0300

    Added BMI calculation

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace CalculadoraIMC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("aaaaaaaaaa");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your height in meters? Ex.: 1,65");
+            float height = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("What is your weight in Kg? Ex.: 70,3");
+            float weight = Convert.ToSingle(Console.ReadLine());
+
+            double bmi = weight / (height * height);
+
+            Console.WriteLine("{0}, your height is {1} and your weight is {2}", name, height, weight);
+            Console.WriteLine("Your BMI is {0}", bmi);
         }
     }
 }
