0d383bc45304eb379d81d0689d4287f076e9a0a6
Baesa <quebedesgamer@gmail.com>
2022-03-29 15:09:42 -0300

    Round BMI and display category

--- Program.cs
+++ Program.cs
@@ -15,10 +15,43 @@ namespace CalculadoraIMC
             Console.WriteLine("What is your weight in Kg? Ex.: 70,3");
             float weight = Convert.ToSingle(Console.ReadLine());
 
-            double bmi = weight / (height * height);
+            double  bmi = Math.Round( weight / (height * height) );
 
             Console.WriteLine("{0}, your height is {1} and your weight is {2}", name, height, weight);
             Console.WriteLine("Your BMI is {0}", bmi);
+
+            if (bmi < 16)
+            {
+                Console.WriteLine("You are Underweight (Severe thinness)");
+            }
+            else if (bmi < 16.9)
+            {
+                Console.WriteLine("You are Underweight (Moderate thinness)");
+            }
+            else if (bmi < 18.4)
+            {
+                Console.WriteLine("You are Underweight (Mild thinness)");
+            }
+            else if (bmi < 24.9)
+            {
+                Console.WriteLine("You are Normal range");
+            }
+            else if (bmi < 29.9)
+            {
+                Console.WriteLine("You are Overweight (Pre-obese)");
+            }
+            else if (bmi < 34.9)
+            {
+                Console.WriteLine("You are Obese (Class I)");
+            }
+            else if (bmi < 39.9)
+            {
+                Console.WriteLine("You are Obese (Class II)");
+            }
+            else
+            {
+                Console.WriteLine("You are Obese (Class III)");
+            }
         }
     }
 }
